a22ec2d6859dfd3dd8b5dc79c0bdbcd76bf8b773
NX <38133441+1NXXXN1@users.noreply.github.com>
2024-09-27 17:16:58 +0500

    Update

--- player/config.js
+++ player/config.js
@@ -5,4 +5,4 @@ const REQUIRED_VERSION = '3.1.0';
 const KINOBOX_API = 'https://kinobox.tv/api/players';
 
 // Available players sources
-const SOURCES = ['alloha', 'ashdi', 'cdnmovies', 'collaps', 'hdvb', 'kodik', 'vibix', 'videocdn', 'voidboost'];
+const SOURCES = ['alloha', 'ashdi', 'cdnmovies', 'collaps', 'hdvb', 'kodik', 'vibix', 'videocdn', 'voidboost', 'ninsel'];
