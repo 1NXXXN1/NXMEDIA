8153131f36bffcdce2be6e5b42462162a6940895
NX <38133441+1NXXXN1@users.noreply.github.com>
2024-09-30 13:57:24 +0500

    Update

--- player/player.js
+++ player/player.js
@@ -293,7 +293,7 @@ function setup() {
 			logger.error('Initialization timeout');
 			document.getElementById("player").style.display = "none";
 			document.getElementById("background").style.opacity = "0.1";
-		}, 5000);
+		}, 2000);
 
 		// Get cached movie key from URL
 		const movieKey = getSearchParam('movie');
